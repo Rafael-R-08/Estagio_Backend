a4994e3615d39d6a15e0fa3ab58c9467b0e9f6d7
Rafael Ribeiro Rodrigues <rafael_rodrigues@pt.softinsa.com>
2026-03-30 14:30:59 +0100

    feat: remove Scraper and Softinsa Learning modules, controllers, services, and DTOs

--- src/softinsa-learning/dto/update-softinsa-learning.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSoftinsaLearningDto } from './create-softinsa-learning.dto';
-
-export class UpdateSoftinsaLearningDto extends PartialType(CreateSoftinsaLearningDto) {}
